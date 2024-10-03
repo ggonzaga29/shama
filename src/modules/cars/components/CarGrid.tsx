512847102ac2670a7e2aeb2543d24d595fb63b09
Gian Gonzaga <gian@giangonzaga.com>
2024-10-03 19:20:08 +0000

    feat: add car grid controls

--- src/modules/cars/components/CarGrid.tsx
+++ src/modules/cars/components/CarGrid.tsx
@@ -1,3 +1,6 @@
+import { Add } from '@carbon/icons-react';
+import { Button } from 'src/components/ui/Button';
+import { Input } from 'src/components/ui/Input';
 import { getAllCars } from 'src/modules/cars/actions';
 import CarGridCard from 'src/modules/cars/components/CarGridCard';
 
@@ -5,8 +8,20 @@ export default async function CarGrid() {
   const cars = await getAllCars();
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-      {cars?.map((car) => <CarGridCard key={car.id} car={car} />)}
+    <div>
+      <div className="mb-4 flex items-center justify-between">
+        <div>
+          <Input placeholder="Search for a vehicle" />
+        </div>
+
+        <Button variant="outline" className="flex items-center gap-2">
+          <Add size={16} />
+          Add Vehicle
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {cars?.map((car) => <CarGridCard key={car.id} car={car} />)}
+      </div>
     </div>
   );
 }
