0d6c94b30f9e607a2ddbc8b2b0b5b0eb43be3b1c
Gian Gonzaga <gian@giangonzaga.com>
2024-10-10 22:41:54 +0000

    feat: change auth layout

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable tailwindcss/enforces-negative-arbitrary-values */
 import { LoginForm } from 'app/auth/components';
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Logo from 'src/assets/Logo';
@@ -15,70 +14,46 @@ import {
 
 export default function AuthPage() {
   return (
-    <section className="flex h-screen">
-      <div className="relative hidden h-full basis-1/2 bg-primary md:block">
-        {/* Overlay */}
-        <div className="absolute z-[2] size-full bg-black/80">
-          <Image
-            src="assets/images/logoDarkWithText.png"
-            alt="Logo"
-            width={150}
-            height={55}
-            className="absolute left-8 top-8"
-          />
+    <div className="relative flex size-full basis-full items-center justify-center md:basis-1/2">
+      <ModeToggle className="absolute right-4 top-4" />
+
+      <div className="w-full max-w-md md:max-w-sm">
+        <div className="flex w-full items-center justify-center">
+          <Logo className="scale-[0.75]" />
         </div>
+        <Card className="w-full border-none shadow-none">
+          <CardHeader>
+            <CardTitle className="text-2xl text-heading-foreground">
+              Login to Shama
+            </CardTitle>
+            <CardDescription>
+              Enter your credentials below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+            <div className="mt-4 text-center text-sm">
+              <Link href="/forgot" className="hover:underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <div className="mt-2 text-center text-sm">
+              Need help?{' '}
+              <Link
+                href="mailto:gian@giangonzaga.com"
+                className="hover:underline"
+              >
+                Contact the Developer
+              </Link>
+            </div>
 
-        {/* Cover Photo */}
-        <Image
-          src="assets/images/car_fleet.avif"
-          alt="Cover Photo"
-          className="absolute z-[1] object-cover"
-          fill
-        />
+            <p className="mt-8 text-center text-xs text-muted-foreground">
+              Signups are by invitation only. If you don&apos;t have an account,
+              please contact Shama Travel & Tours or gian@giangonzaga.com.
+            </p>
+          </CardContent>
+        </Card>
       </div>
-
-      <div className="relative flex size-full basis-full items-center justify-center md:basis-1/2">
-        <ModeToggle className="absolute right-4 top-4" />
-
-        <div className="w-full max-w-md md:max-w-sm">
-          <div className="flex w-full items-center justify-center">
-            <Logo className="scale-[0.75]" />
-          </div>
-          <Card className="w-full border-none shadow-none">
-            <CardHeader>
-              <CardTitle className="text-2xl text-heading-foreground">
-                Login to Shama
-              </CardTitle>
-              <CardDescription>
-                Enter your credentials below to login to your account.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <LoginForm />
-              <div className="mt-4 text-center text-sm">
-                <Link href="/forgot" className="hover:underline">
-                  Forgot your password?
-                </Link>
-              </div>
-              <div className="mt-2 text-center text-sm">
-                Need help?{' '}
-                <Link
-                  href="mailto:gian@giangonzaga.com"
-                  className="hover:underline"
-                >
-                  Contact the Developer
-                </Link>
-              </div>
-
-              <p className="mt-8 text-center text-xs text-muted-foreground">
-                Signups are by invitation only. If you don&apos;t have an
-                account, please contact Shama Travel & Tours or
-                gian@giangonzaga.com.
-              </p>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-    </section>
+    </div>
   );
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image';
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <section className="flex h-screen">
+      <div className="relative hidden h-full basis-1/2 bg-primary md:block">
+        {/* Overlay */}
+        <div className="absolute z-[2] size-full bg-black/80">
+          <Image
+            src="assets/images/logoDarkWithText.png"
+            alt="Logo"
+            width={150}
+            height={55}
+            className="absolute left-8 top-8"
+          />
+        </div>
+
+        {/* Cover Photo */}
+        <Image
+          src="assets/images/car_fleet.avif"
+          alt="Cover Photo"
+          className="absolute z-[1] object-cover"
+          fill
+        />
+      </div>
+
+      <div className="size-full basis-full md:basis-1/2">{children}</div>
+    </section>
+  );
 }
